a1291a34ff3d0d32ced6f319baf4c9f38e39387e
Fredrik Josefsson <fredrik@superpanic.com>
2020-11-21 12:42:17 +0100

    Initial Commit

---
+++ CapsUp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CapsUp
+//
+//  Created by Fredrik Josefsson on 2020-11-21, 12:42.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+	
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ CapsUp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CapsUp
+//
+//  Created by Fredrik Josefsson on 2020-11-21, 12:42.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
